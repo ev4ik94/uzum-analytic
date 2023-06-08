3ea6aa4195ab8cf5878e608cbd9d191373a9d2ff
ev4ik94 <evelina9449@gmail.com>
2023-06-09 01:20:29 +0500

    update

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,24 +6,27 @@ console.log(process.env.USER)
 console.log(process.env.PASSWORD)
 
 
-//export const sequelize = new Sequelize(`postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT_DB}/${process.env.DB_NAME}`);
+const DATABASE_URL=`postgresql://${process.env.USER!}:${process.env.PASSWORD!}@${process.env.HOST}:${process.env.PORT_DB}/${process.env.DB_NAME}?schema=public`
 
-export const sequelize =  new Sequelize(
-    process.env.DB_NAME!,
-    process.env.USER!,
-    process.env.PASSWORD!,
 
-    {
-        dialect: 'postgres',
-        host: process.env.HOST!,
-        port: +process.env.PORT_DB!,
-        dialectOptions: {
-            ssl: {
-                required: true,
-                rejectUnauthorized: false
-            },
+export const sequelize = new Sequelize(DATABASE_URL);
 
-        }
-
-    }
-)
+// export const sequelize =  new Sequelize(
+//     process.env.DB_NAME!,
+//     process.env.USER!,
+//     process.env.PASSWORD!,
+//
+//     {
+//         dialect: 'postgres',
+//         host: process.env.HOST!,
+//         port: +process.env.PORT_DB!,
+//         dialectOptions: {
+//             ssl: {
+//                 required: true,
+//                 rejectUnauthorized: false
+//             },
+//
+//         }
+//
+//     }
+// )
