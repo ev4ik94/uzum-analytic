2dd0ceea6bacd6ee7d1e4aa8984cdac2d6ae9c7f
ev4ik94 <evelina9449@gmail.com>
2023-06-08 19:13:43 +0500

    update

--- src/db/index.ts
+++ src/db/index.ts
@@ -14,7 +14,6 @@ export const sequelize =  new Sequelize(
 
     {
         dialect: 'postgres',
-        native: true,
         ssl: true,
         host: process.env.HOST!,
         port: +process.env.PORT_DB!,
