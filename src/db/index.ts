7b1bae5fc0d774ab95d232be7e4f16edf8bdacb1
ev4ik94 <evelina9449@gmail.com>
2023-06-08 15:24:01 +0500

    update

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,7 @@ import {ConfigService} from "../config/config.service";
 
 const config = new ConfigService()
 console.log(process.env.USER)
+console.log(config.get('USER'))
 
 
 export const sequelize = new Sequelize(`postgres://${config.get('USER')}:${config.get('PASSWORD')}@${config.get('HOST')}:${config.get('PORT_DB')}/${config.get('DB_NAME')}`);
