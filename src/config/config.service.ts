3c0569e18d612c404f88c7860f370c6d45f9f594
ev4ik94 <evelina9449@gmail.com>
2023-06-08 15:45:38 +0500

    update

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -8,23 +8,16 @@ export class ConfigService implements IConfig{
     constructor() {
         const {error, parsed} = config();
 
+
         if(error){
-
-
-            //throw new Error('Не найден файл .env')
+            throw new Error('Не найден файл .env')
         }
 
-        if(parsed) this.configs = parsed
-        //@ts-ignore
-        else this.configs = process.env
-        // //@ts-ignore
-        // if(process.env)
-        //
-        // if(!parsed){
-        //     //throw new Error('Пустой файл .env')
-        // }
-        //
-        // if(parsed)
+        if(!parsed){
+            throw new Error('Пустой файл .env')
+        }
+
+        this.configs = parsed
     }
 
     get(key:string):string{
