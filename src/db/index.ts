55ccfc76e834cea6f3d8949b38bc8d23250a1cea
ev4ik94 <evelina9449@gmail.com>
2023-06-08 16:57:00 +0500

    update

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,5 +5,17 @@ dotenv.config()
 console.log(process.env.USER)
 
 
-export const sequelize = new Sequelize(`postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT_DB}/${process.env.DB_NAME}`);
+//export const sequelize = new Sequelize(`postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT_DB}/${process.env.DB_NAME}`);
 
+export const sequelize =  new Sequelize(
+    process.env.DB_NAME!,
+    process.env.USER!,
+    process.env.PASSWORD!,
+    {
+        dialect: 'postgres',
+        host: process.env.HOST!,
+        port: +process.env.PORT_DB!,
+        ssl: true
+
+    }
+)
