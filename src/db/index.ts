774960b3caee3605cc60c66b68efd3fb76d30dd8
ev4ik94 <evelina9449@gmail.com>
2023-06-09 15:06:35 +0500

    update

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,27 +5,27 @@ dotenv.config()
 
 
 
-const DATABASE_URL=`postgresql://${process.env.USER!}:${process.env.PASSWORD!}@${process.env.HOST}:${process.env.PORT_DB}/${process.env.DB_NAME}?schema=public&ssl=true`
+const DATABASE_URL=`postgresql://${process.env.USER!}:${process.env.PASSWORD!}@${process.env.HOST}:${process.env.PORT_DB}/${process.env.DB_NAME}?schema=public`
 
 
-export const sequelize = new Sequelize(DATABASE_URL);
+//export const sequelize = new Sequelize(DATABASE_URL);
 
-// export const sequelize =  new Sequelize(
-//     process.env.DB_NAME!,
-//     process.env.USER!,
-//     process.env.PASSWORD!,
-//
-//     {
-//         dialect: 'postgres',
-//         host: process.env.HOST!,
-//         port: +process.env.PORT_DB!,
-//         dialectOptions: {
-//             ssl: {
-//                 required: true,
-//                 rejectUnauthorized: false
-//             },
-//
-//         }
-//
-//     }
-// )
+export const sequelize =  new Sequelize(
+    process.env.DB_NAME!,
+    process.env.USER!,
+    process.env.PASSWORD!,
+
+    {
+        dialect: 'postgres',
+        host: process.env.HOST!,
+        port: +process.env.PORT_DB!,
+        dialectOptions: {
+            ssl: {
+                required: true,
+                rejectUnauthorized: false
+            },
+
+        }
+
+    }
+)
