3668718156fd5168b78fcf5fa07fb97f840f89e6
ev4ik94 <evelina9449@gmail.com>
2023-06-09 15:03:54 +0500

    update

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 
 
-const DATABASE_URL=`postgresql://${process.env.USER!}:${process.env.PASSWORD!}@${process.env.HOST}:${process.env.PORT_DB}/${process.env.DB_NAME}?schema=public`
+const DATABASE_URL=`postgresql://${process.env.USER!}:${process.env.PASSWORD!}@${process.env.HOST}:${process.env.PORT_DB}/${process.env.DB_NAME}?schema=public&ssl=true`
 
 
 export const sequelize = new Sequelize(DATABASE_URL);
