62983cb6f806ce209065b7f03cee3275d4ce6b72
ev4ik94 <evelina9449@gmail.com>
2023-06-18 15:53:52 +0500

    update

--- src/app.ts
+++ src/app.ts
@@ -48,14 +48,15 @@ class Bot{
         this.bot.use(async(ctx, next)=>{
 
 
-console.log(ctx.session)
-console.log(this.user_auth)
+
             //@ts-ignore
             if(ctx.session.token==='ZvF2QEdLAhGxEOXeM3yO0KKmOOM') {
                 console.log('CLEAR')
                 //@ts-ignore
                 ctx.session = null
             }
+            console.log(ctx.session)
+            console.log(this.user_auth)
             if(ctx.session.token){
 
 
