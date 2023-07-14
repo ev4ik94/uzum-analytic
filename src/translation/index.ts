5e596aba8c5d67f44d4efa97ecbe7708e5828cc8
ev4ik94 <evelina9449@gmail.com>
2023-07-14 09:59:57 +0500

    update

--- src/translation/index.ts
+++ src/translation/index.ts
@@ -1,5 +1,5 @@
-const ru = require('./ru.json')
-const uz = require('./uz.json')
+const ru = require('../../translation/ru.json')
+const uz = require('../../translation/uz.json')
 
 module.exports = {ru, uz}
 
