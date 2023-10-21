843a17782c559a484aa9123bf0b4caf5d3cd7d8a
ev4ik94 <evelina9449@gmail.com>
2023-10-21 19:18:44 +0500

    update

--- src/data.ts
+++ src/data.ts
@@ -4,7 +4,7 @@ export const EVA_TOKEN:string = 'yz_TtM9MRXM1Ehm_-fojn5ER-_k'
 export const RAFA_TOKEN:string = 'cgxeiH9yY6PB-Q8XN6KImMnWfWU'
 
 
-export const USERS_DATA = {
+export const USERS_DATA:any = {
     'eva_4eva': {
         username: '998935531250',
         password: '41f733EvA_RaFa'
